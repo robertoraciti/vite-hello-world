7ba6c58ac615e2fa7744f24d1f355497a1881100
robertoraciti <rob.raciti97@gmail.com>
2023-09-07 14:21:57 +0200

    Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 
 export default {
   data() {
@@ -10,12 +11,14 @@ export default {
 
   components: {
     AppTitle,
+    AppHero,
   },
 };
 </script>
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      image: "../assets/img/05.webp",
+    };
+  },
+};
+</script>
+
+<template>
+  <img src="../assets/img/05.webp" alt="" srcset="" />
+</template>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+}
+</style>
